ef66f566a0578239e85562a0c66b1c92e424d3b5
wangzhiqiang <40115555+zhiqw@users.noreply.github.com>
2021-05-23 15:39:36 +0800

    Update install-docker.sh

--- install-docker.sh
+++ install-docker.sh
@@ -29,6 +29,6 @@ echo "{
 systemctl start docker
 
 # docker-composer
-curl -L "https://github.com/docker/compose/releases/download/1.23.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+curl -L "https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 
